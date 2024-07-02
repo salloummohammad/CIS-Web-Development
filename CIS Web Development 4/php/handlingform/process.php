436a64fbc5dc79436bfc0abe10c1480dfc6684a5
salloummohammad <mohammad2salloum@gmail.com>
2024-07-03 01:51:45 +0300

    b
    n

---
+++ CIS Web Development 4/php/handlingform/process.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST["submit"])){
+    $pn=$_POST["pname"];
+    $pr = $_POST["price"];
+    $q = $_POST["qty"];
+    $cl = $_POST["color"];
+
+    $link = mysqli_connect("localhost", "root", "", "supermarket1");
+
+    $sql = "INSERT INTO product(pro_name, price, qty, color) VALUES($pn, $pr, $q, $cl)";
+
+    if(mysqli_query($link, $sql)){
+        echo "Record inserted successfully!";
+    }else{
+        echo "Something went wrong!";
+    }
+}
+?> 
